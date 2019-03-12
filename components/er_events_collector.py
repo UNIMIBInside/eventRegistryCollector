b4175efa0169da39e15b256b18ab2fd727f99a54
Andrea Sartori <a.sartori8@campus.unimib.it>
2019-03-12 12:36:24 +0100

    Minor changes in code organization

--- components/er_events_collector.py
+++ components/er_events_collector.py
@@ -15,10 +15,33 @@ def init_connection_er(access_token):
     
     return er
 
-def get_events_by_location(access_token, location):
+def get_events_by_location(access_token):
 
     # establishing connection with Event Register
     er = init_connection_er(access_token)
+
+    # ER query filtering
+    filters = {}
+    print("\nSpecify filtering, leave blank if you do not want set a filter")
+    print("Location:", end=" ")
+    
+    # Filter location
+    filters["location"] = input()
+
+    # Filter event date
+    date_valid = False
+
+    while not date_valid:
+        print("Event date (yyyy-MM-dd):", end=" ")
+        filters["eventDate"] = input()
+    
+        pattern_date = re.compile("([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))")
+        match_date = pattern_date.match(filters["eventDate"])
+
+        if match_date is not None:
+            date_valid = True
+        else:
+            print("[ERROR] Date invalid or input date format is not recognized")
 
     # set query to get events
     q = QueryEvents(
@@ -59,28 +82,5 @@ if __name__ == "__main__":
     # ER API key input request
     print("Please, specify EventRegistry API Key:", end=' ')
     er_api_key = input()
-
-    # ER query filtering
-    filters = {}
-    print("\nSpecify filtering, leave blank if you do not want set a filter")
-    print("Location:", end=" ")
-    
-    # Filter location
-    filters["location"] = input()
-
-    # Filter event date
-    date_valid = False
-
-    while not date_valid:
-        print("Event date (yyyy-MM-dd):", end=" ")
-        filters["eventDate"] = input()
-    
-        pattern_date = re.compile("([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))")
-        match_date = pattern_date.match(filters["eventDate"])
-
-        if match_date is not None:
-            date_valid = True
-        else:
-            print("[ERROR] Date invalid or input date format is not recognized")
-    
-    get_events_by_location(er_api_key, filters)
+   
+    get_events_by_location(er_api_key)
